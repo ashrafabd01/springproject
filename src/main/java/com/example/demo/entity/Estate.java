33273b37300995f7ca03cbb2ccfe4f7545b1bd40
ashrafabd01 <ashraf.king1006@gmail.com>
2021-11-24 17:03:06 +0200

    init point

---
+++ src/main/java/com/example/demo/entity/Estate.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.With;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+@Entity
+public class Estate {
+    @Id
+    @GeneratedValue
+    Integer id;
+    String number;
+    String space;
+    String type;
+    String addressdetails;
+    String description;
+}
